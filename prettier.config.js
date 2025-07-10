5c4f54f9518b3fcf1f2ea773978ad8fecc7e1945
Ivan <karachun04@gmail.com>
2025-07-10 14:41:39 +0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  singleAttributePerLine: false,
+};
